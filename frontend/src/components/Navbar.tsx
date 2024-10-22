eaf1cca62c38a1ee2ae317793f28d7c1ff12b171
manzitresor <manzitre@gmail.com>
2024-10-22 21:37:39 +0200

    refactor: customize navbar using navlink and some styles

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,26 +3,30 @@ import  search from "../assets/Search Icon.svg"
 import profile from "../assets/Account.svg";
 import favorite from "../assets/Favorite.svg"
 import cart from "../assets/Icon.svg"
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between">
-      <div className="flex gap-x-16 items-center">
-        <img src={menu} alt="menu-icon" width={24}/>
-        <div className="bg-gray-200 flex rounded-lg items-center w-80 h-10 gap-x-5 px-3">
-          <img src={search} alt="search-icon" width={16}/>
-          <p className="text-lg text-gray-500">What are you looking for ?</p>
+    <header className="max-w-screen-2xl mx-auto px-4 py-6">
+      <nav className="flex justify-between items-center">
+        <div className="flex gap-x-16 items-center">
+          <NavLink to='/'>
+          <img src={menu} alt="menu-icon" width={24}/>
+          </NavLink>
+          <div className=" relative flex  items-center w-80 h-10 gap-x-5 px-3">
+            <img src={search} alt="search-icon" className="absolute left-5 w-4"/>
+            <input type="text" placeholder="What are you looking for ?f" className="text-lg text-gray-500 bg-secondary rounded-lg py-2 px-6 md:px-8 focus:outline-none"></input>
+          </div>
         </div>
-      </div>
-      <div className="flex gap-x-5">
-        <img src={profile} alt="profile"  width={18}/>
-        <img src={favorite} alt = 'favorite' width={25}/>
-        <div className="bg-primary flex items-center justify-evenly gap-x-3 rounded-lg w-36">
-          <img src={cart} alt="shopping cart" width={20}/>
-          <p className="text-white">Basket</p>
+        <div className="flex gap-x-5">
+          <img src={profile} alt="profile"  width={18}/>
+          <img src={favorite} alt = 'favorite' width={25}/>
+          <div className="bg-primary flex items-center justify-evenly gap-x-3 rounded-lg w-36">
+            <img src={cart} alt="shopping cart" width={20}/>
+            <p className="text-white">Basket</p>
+          </div>
         </div>
-      </div>
-    </nav>
-
+      </nav>
+    </header>
   )
 }
