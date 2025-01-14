a71c3a9207d552b35b20183f2943f412615e5369
manzitresor <manzitre@gmail.com>
2025-01-14 16:20:07 +0200

    chore: add dynamic data

---
+++ frontend/src/data/news.js
@@ -0,0 +1,31 @@
+import newsImage1 from '../../news/news-1.png';
+import newsImage2 from '../../news/news-2.png';
+import newsImage3 from '../../news/news-3.png';
+import newsImage4 from '../../news/news-4.png';
+
+export const news= [
+    {
+        id: 1,
+        title: "Global Climate Summit Calls for Urgent Action",
+        description: "World leaders gather at the Global Climate Summit...",
+        image: newsImage1
+    },
+    {
+        id: 2,
+        title: "Breakthrough in AI Technology Announced",
+        description: "A major breakthrough in artificial intelligence...",
+        image: newsImage2
+    },
+    {
+        id: 3,
+        title: "New Space Mission Aims to Explore Distant Galaxies",
+        description: "NASA has unveiled plans for a new space mission...",
+        image: newsImage3
+    },
+    {
+        id: 4,
+        title: "Stock Markets Reach Record Highs Amid Economic Recovery",
+        description: "Global stock markets have reached record highs...",
+        image: newsImage4
+    },
+];
