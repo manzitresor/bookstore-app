6a022ea8a263d1f67f7dfd75b816ca4d78302e7f
manzitresor <manzitre@gmail.com>
2025-01-17 11:30:44 +0200

    chore: add login google button

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -1,17 +1,19 @@
 import { Checkbox } from "flowbite-react";
+import { Link } from "react-router-dom";
+import { FaGoogle } from "react-icons/fa";
 
 export default function Login() {
   return (
     <div className="w-1/3 shadow-lg mx-auto p-11">
-      <h3 className="">Please Login</h3>
+      <h3 className="text-semi">Please Login</h3>
       <form>
         <div className="flex flex-col my-6">
             <label>Email</label>
-            <input type="text" placeholder="Email Address" className="border border-gray-300 p-2 rounded-lg focus:outline-none"/>
+            <input type="email" id='email' placeholder="Email Address" className="border border-gray-300 p-2 rounded-lg focus:outline-none"/>
         </div>
         <div className="flex flex-col my-6">
             <label>Password</label>
-            <input type="password" placeholder="Password" className="border border-gray-300 p-2 rounded-lg focus:outline-none"/>
+            <input type="password" id='password' placeholder="Password" className="border border-gray-300 p-2 rounded-lg focus:outline-none"/>
         </div>
         <div>
             <Checkbox></Checkbox>
@@ -19,6 +21,11 @@ export default function Login() {
         </div>
         <button type="submit" className="bg-primary px-5 py-2 rounded-lg text-white mt-2">Submit</button>
       </form>
+      <p className="text-sm my-3 text-gray-500">Havan't an account ? Please <Link to={'/register'} className="text-blue-500">Register</Link></p>
+      <button className="flex gap-x-3 items-center bg-black text-white p-2 w-full justify-center">
+        <FaGoogle></FaGoogle>
+        Sign in with Google
+      </button>
     </div>
   )
 }
