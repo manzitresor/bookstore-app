9fc5355fbc545534252cc2cd4d6d46acd7b96932
manzitresor <manzitre@gmail.com>
2025-01-26 20:21:29 +0200

    chore: add functionality of add cart

--- frontend/src/redux/features/cart/cartSlice.ts
+++ frontend/src/redux/features/cart/cartSlice.ts
@@ -2,10 +2,14 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 
 interface cartItem {
-    _id: string
-    name: string
-    price: number
-    quantity: number
+   _id: number;
+  title: string;
+  description: string;
+  category: string;
+  trending: boolean;
+  coverImage: string;
+  oldPrice: number;
+  newPrice: number;
 }
 
 interface cartState {
@@ -17,13 +21,14 @@ const initialState: cartState = {
 }
 
 const cartSlice = createSlice({
-    name: 'cart',
+    name: 'addCart',
     initialState,
     reducers: {
         addCart: (state,action: PayloadAction<cartItem>)=>{
             const existingItem = state.cartItems.find(item => item._id === action.payload._id)
             if(!existingItem) {
-              initialState.cartItems.push(action.payload)
+                state.cartItems=[...state.cartItems,action.payload]
+                console.log("working",action.payload)
               alert('Items succesful added')
             } else {
                 alert('Item already exist')
@@ -31,5 +36,7 @@ const cartSlice = createSlice({
         }
     }
 })
+
+export const { addCart } = cartSlice.actions
 
 export default cartSlice.reducer
