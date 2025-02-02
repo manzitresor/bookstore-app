15e7662dd6331f85de72a8f3e5fd0bce846da2f9
manzitresor <manzitre@gmail.com>
2025-02-02 17:35:38 +0200

    chore: add styles for add cart card

--- frontend/src/pages/cart/AddCart.tsx
+++ frontend/src/pages/cart/AddCart.tsx
@@ -1,3 +1,4 @@
+import { getImgUrls } from "../../utils/getImgUrls"
 import { Book } from "../home/TopSeller"
 
 interface cartItemProp {
@@ -7,8 +8,21 @@ interface cartItemProp {
 
 export default function AddCart({items}:cartItemProp) {
   return (
-    <div>
-        <h1>{items.title}</h1>
+    <div className="shadow-xl p-7">
+      <div className="grid grid-cols-12 items-center">
+      <img src={`${getImgUrls(items.coverImage)}`} className="col-span-2"></img>
+      <div className="space-y-6 col-span-10">
+          <div className="flex justify-between">
+            <h1 className="font-semibold">{items.title}</h1>
+            <p className="font-bold">${items.newPrice}</p>
+          </div>
+            <p className="font-semibold">Category: <span className="font-normal">{items.category}</span></p>
+          <div className="flex justify-between">
+            <p className="font-semibold">Qty: <span className="font-normal">1</span></p>
+            <button className="text-blue-800 cursor-pointer hover:text-red-300">Remove</button>
+          </div>
+      </div>
+      </div>
     </div>
   )
 }
--- frontend/src/pages/cart/Cart.tsx
+++ frontend/src/pages/cart/Cart.tsx
@@ -5,6 +5,10 @@ export default function Cart() {
     const cartItems = useSelector((state: RootState)=> state.addCart.cartItems)
   return (
     <div>
+      <div className="flex justify-between mx-6">
+        <h3 className="text-lg">Shopping cart</h3>
+        <button className="bg-primary p-2 rounded-lg text-white">Clear Cart</button>
+      </div>
       {
         cartItems.map( items =>(
             <AddCart key={items._id} items= {items}/>
