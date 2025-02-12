ca69034a4ca452216f48b453252efa85b4299566
manzitresor <manzitre@gmail.com>
2025-02-12 12:36:01 +0200

    chore: add chakra Ui for tost

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,10 +4,22 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './routers/router'
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
+const theme = extendTheme({
+  styles: {
+    global: () => ({
+      body: {
+        bg: "gray.50",
+      },
+    }),
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
-  <Provider store={store} >
-    <RouterProvider router={router}/>
-  </Provider>,
+  <ChakraProvider theme={theme}>
+    <Provider store={store} >
+      <RouterProvider router={router}/>
+    </Provider>,
+  </ChakraProvider>
 )
